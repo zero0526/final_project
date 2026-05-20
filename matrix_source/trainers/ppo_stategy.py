eff62be18ea8f96b11491e7d49786581c7e4f715
khoe <nguyenkhoe2k4@gmail.com>
2026-05-20 07:46:00 +0700

    change config

--- matrix_source/trainers/ppo_stategy.py
+++ matrix_source/trainers/ppo_stategy.py
@@ -37,7 +37,7 @@ class PPOStrategy(AlgorithmStrategy):
         # Hyperparams from user
         self.lower_cfg = {'min_size': 4096, 'batch': 128, 'epochs': 6}
         self.upper_cfg = {'min_size': 512, 'batch': 64, 'epochs': 4}
-        self.lower_warmup_steps = 60
+        self.lower_warmup_steps = 40
         self.upper_warmup_steps = 60
         self.alt_steps = 60
 
