c2f79709d8c6a4d6862357082380981358a04ecb
khoenv <nguyenkhoe2k4@gmail.com>
2026-06-08 18:15:45 +0700

    udpate

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from matrix_source.trainers.semi_distribute_task_group import GroupGRUPPOSCAFFOL
 from matrix_source.trainers.coma_residual import COMAResidualStrategy
 
 from matrix_source.configs.configs import cfg
-cfg.hyper_neural["NUM_LOWER_AGENTS"]= 20
+cfg.hyper_neural["NUM_LOWER_AGENTS"]= 300
 
 if __name__ == '__main__':
-    strategy = GroupGRUPPOSCAFFOLDREPStrategy()
+    strategy = COMAResidualStrategy()
     trainer = Trainer(strategy=strategy)
     trainer.train()
